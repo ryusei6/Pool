f5cf07617093421e47494eb830f7d45cc2b34bc7
ryusei6 <e1n5te1nrrrrr@gmail.com>
2021-02-12 20:49:49 +0900

    target nameだけで処理できるようにした, tweak

--- scraping/data/extend.py
+++ scraping/data/extend.py
@@ -4,31 +4,28 @@ import argparse
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 
+IMGS_ROOT_DIR = './imgs'
+EXTEND_NUM = 50
+RESIZE_SHAPE = (150, 150)
+
 
 # 画像を拡張する関数
 def draw_images(generator, x, index, output_dir):
     save_name = 'extened_' + str(index).zfill(4)
-    g = generator.flow(x, batch_size=1, save_to_dir=output_dir, save_prefix=save_name, save_format='jpeg')
+    g = generator.flow(x, batch_size=1, save_to_dir=output_dir, save_prefix=save_name, save_format='jpg')
 
     # 1つの入力画像から何枚拡張するかを指定（今回は50枚）
-    for i in range(50):
+    for i in range(EXTEND_NUM):
         bach = g.next()
 
 
-def extend_img(path_to_imgs_dir):
-    if not os.path.isdir(path_to_imgs_dir):
+def extend_img(target):
+    target_dir = os.path.join(IMGS_ROOT_DIR, 'face', target)
+    if not os.path.isdir(target_dir):
         print('directory not found')
         return
-    input_dirs = os.path.split(path_to_imgs_dir)
-    if not input_dirs[1]:
-        input_dirs = os.path.split(input_dirs[0])
-    path_to_original_dir, target = input_dirs
-    image_names = os.listdir(path_to_imgs_dir)
-    images_num = len(image_names)
-    path_to_root_dir = os.path.split(path_to_original_dir)[0]
-    path_to_extended_dir = os.path.join(path_to_root_dir, 'extended', target)
-    os.makedirs(path_to_extended_dir, exist_ok=True)
-
+    extended_dir = os.path.join(IMGS_ROOT_DIR, 'extended', target)
+    os.makedirs(extended_dir, exist_ok=True)
 
     datagen = ImageDataGenerator(
         rotation_range=30,
@@ -39,20 +36,21 @@ def extend_img(path_to_imgs_dir):
         vertical_flip=True
     )
 
+    imgs_path = glob.glob(target_dir + '/*.jpg')
     # 読み込んだ画像を順に拡張
-    for i in range(images_num):
-        img = load_img(os.path.join(path_to_imgs_dir, image_names[i]))
-        img = img.resize((150, 150))
+    for i in range(len(imgs_path)):
+        img = load_img(imgs_path[i])
+        img = img.resize(RESIZE_SHAPE)
         x = img_to_array(img)
         x = np.expand_dims(x, axis=0)
-        draw_images(datagen, x, i, path_to_extended_dir)
+        draw_images(datagen, x, i, extended_dir)
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--directory', help='input images location', type=str, required=True)
+    parser.add_argument('-t', '--target', help='target name', type=str, required=True)
     args = parser.parse_args()
-    path_to_imgs = args.directory
-    extend_img(path_to_imgs)
+    extend_img(args.target)
 
 
 if __name__ == '__main__':
