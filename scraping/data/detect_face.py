e0813aba3dd3fd3e1ff13a44d054d3fea263d188
ryusei6 <e1n5te1nrrrrr@gmail.com>
2021-02-12 20:48:58 +0900

    target nameだけで処理できるようにした, tweak

--- scraping/data/detect_face.py
+++ scraping/data/detect_face.py
@@ -1,34 +1,36 @@
 # -*- coding:utf-8 -*-
+import os
+import glob
+import argparse
+
 import cv2
 import numpy as np
-import os
-import argparse
+
 
 cascade_path = './haarcascades/haarcascade_frontalface_default.xml'
 faceCascade = cv2.CascadeClassifier(cascade_path)
 
+IMGS_ROOT_DIR = './imgs'
 
-def detect_face(path_to_imgs_dir):
-    if not os.path.isdir(path_to_imgs_dir):
+
+def detect_face(target):
+    target_dir = os.path.join(IMGS_ROOT_DIR, 'original', target)
+    if not os.path.isdir(target_dir):
         print('directory not found')
         return
-    input_dirs = os.path.split(path_to_imgs_dir)
-    if not input_dirs[1]:
-        input_dirs = os.path.split(input_dirs[0])
-    path_to_original_dir, target = input_dirs
-    images_num = len(os.listdir(path_to_imgs_dir))
-    path_to_root_dir = os.path.split(path_to_original_dir)[0]
-    path_to_face_dir = os.path.join(path_to_root_dir, 'face', target)
-    path_to_detected_dir = os.path.join(path_to_root_dir, 'detected', target)
-    os.makedirs(path_to_face_dir, exist_ok=True)
-    os.makedirs(path_to_detected_dir, exist_ok=True)
+    face_dir = os.path.join(IMGS_ROOT_DIR, 'face', target)
+    detected_dir = os.path.join(IMGS_ROOT_DIR, 'detected', target)
+    os.makedirs(face_dir, exist_ok=True)
+    os.makedirs(detected_dir, exist_ok=True)
 
     error = 0
     file_num = 1
     error_max = 100
-    while file_num - 1 < images_num:
+    imgs = glob.glob(os.path.join(target_dir, '*.jpg'))
+    imgs_num = len(imgs)
+    while file_num - 1 < imgs_num:
         try:
-            path_to_img = os.path.join(path_to_imgs_dir, str(file_num).zfill(4) + '.jpg')
+            path_to_img = os.path.join(target_dir, str(file_num).zfill(4) + '.jpg')
             img = cv2.imread(path_to_img, cv2.IMREAD_COLOR)
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             faces = faceCascade.detectMultiScale(gray, 1.1, 3)
@@ -36,13 +38,13 @@ def detect_face(path_to_imgs_dir):
             if len(faces) > 0:
                 for face_num, rect in enumerate(faces):
                     cv2.rectangle(img2, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (0, 0,255), thickness=1)
-                    cv2.imwrite(os.path.join(path_to_detected_dir, str(file_num) + '.jpg'), img2)
+                    cv2.imwrite(os.path.join(detected_dir, str(file_num) + '.jpg'), img2)
                     print('-> Cutting image', str(file_num).zfill(4) + '_' + str(face_num+1) + '.jpg', end=' ')
                     x = rect[0]
                     y = rect[1]
                     w = rect[2]
                     h = rect[3]
-                    cv2.imwrite(os.path.join(path_to_face_dir, str(file_num).zfill(4) + '_' + str(face_num+1) + '.jpg'), img[y:y+h, x:x+w])
+                    cv2.imwrite(os.path.join(face_dir, str(file_num).zfill(4) + '_' + str(face_num+1) + '.jpg'), img[y:y+h, x:x+w])
                     print('successful')
             else:
                 print('-> ' + str(file_num).zfill(4) + '.jpg: No Face')
@@ -56,13 +58,12 @@ def detect_face(path_to_imgs_dir):
                 break
             continue
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--directory', help='cutted images location', type=str, required=True)
+    parser.add_argument('-t', '--target', help='target name', type=str, required=True)
     args = parser.parse_args()
-    directory_name = args.directory
-
-    detect_face(directory_name)
+    detect_face(args.target)
 
 
 if __name__ == '__main__':
