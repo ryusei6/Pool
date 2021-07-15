94ccca9769c44ca0b472cba979b16fe1c31f7616
ryusei6 <e1n5te1nrrrrr@gmail.com>
2021-07-16 01:18:28 +0900

    tweak

--- spotify-music-search-app/src/components/Login.js
+++ spotify-music-search-app/src/components/Login.js
@@ -4,7 +4,7 @@ import {accessUrl} from "./Spotify";
 function Login() {
     return (
         <div className='Login'>
-            <h2>spotifyへログインしてください。</h2>
+            <h2>承認されたユーザでspotifyへログインしてください。</h2>
             <a href={accessUrl} style={{color: 'white'}}>ログイン</a>
         </div>
     )
